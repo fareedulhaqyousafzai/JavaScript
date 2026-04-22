5d214cdc52a4a50490fc14ab3250a59022f70f57
Fareed UL Haq Yousafzai <fareedulhaqyousafzai@gmail.com>
2026-04-22 14:05:45 +0500

    console

---
+++ console.js
@@ -0,0 +1,15 @@
+let a = 20;
+let b = 20;
+let c = a + b;
+console.log(c);
+
+console.log("PAKITASN");
+console.log(30);
+console.log(["Afatb", "Rehan", "Furqan"]);
+console.log(15 + 20);
+
+console.table(["Afatb", "Rehan", "Furqan"]);
+console.warn("This is Just warning");
+console.error("Spmething went wrong");
+console.time("Test");
+console.timeEnd("Test");
