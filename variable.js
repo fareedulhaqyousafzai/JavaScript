aa6e4aa07516291ee03d4f19d8fbceb0bb9d3070
Fareed UL Haq Yousafzai <fareedulhaqyousafzai@gmail.com>
2026-04-23 14:03:32 +0500

    variable

---
+++ variable.js
@@ -0,0 +1,49 @@
+
+
+//var
+        console.log("var");
+        var name= "fareed";
+        console.log(name);
+        var name="Farhan";
+        console.log(name);
+         name="Kamran";
+        console.log(name);
+
+        //let
+
+        console.log("let");
+        let student= "Aftab";
+        console.log(student);
+         student= "Sohil";
+        console.log(student);
+
+        // const
+
+        console.log("const");
+        const Emp= "Umer";
+        console.log(Emp);
+        
+
+ 
+        // valid variable name
+        
+            console.log("valid variable name");
+var _myvar=12;
+console.log(_myvar)
+
+var $ocity="Karachi";
+console.log($ocity)
+
+var myName="Fareed";
+console.log(myName)
+
+
+   // invalid variable name
+
+ console.log("Invalid variable name");
+
+// var 123var="Fareed";
+// console.log(123var)
+
+// var my@email="Fareed@gmai.com";
+// console.log(my@email)
